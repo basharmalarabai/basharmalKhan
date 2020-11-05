ee2981a7aa429e00092d5f3bf069f14e1bb50217
basharmalarabai <73264236+basharmalarabai@users.noreply.github.com>
2020-11-05 12:11:45 -0500

    Update Bk.java

--- src/YoutubePract/Bk.java
+++ src/YoutubePract/Bk.java
@@ -8,9 +8,12 @@ public class Bk {
 		double y = 2000;
 		System.out.println(y);
 		double z = 2000.50;
-		System.out.println();
+		System.out.println(z);
 		
-		
+		double e = 2000;
+		System.out.println(e);
+		double x = 2000.50;
+		System.out.println(x);
 		
 		
 		
